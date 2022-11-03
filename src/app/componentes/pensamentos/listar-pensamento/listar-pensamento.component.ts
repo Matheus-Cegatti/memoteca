2e9458a141f4bea7c8b37330a100ae2a35951393
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-11-03 15:19:38 -0300

    criando o arquivo db.json e iniciando o arquivo service

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,3 +1,4 @@
+import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,22 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Comunicação entre componentes',
-    autoria: 'Angular',
-    modelo: 'modelo3'
-    },
-    {
-      conteudo: 'Minha prorpriedade é decorada com ()Input',
-    autoria: 'Angular',
-    modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing',
-    autoria: 'Angular',
-    modelo: 'modelo1'
-    },
+  listaPensamentos: Pensamento[] = [
+    // {
+    //   conteudo: 'Comunicação entre componentes',
+    // autoria: 'Angular',
+    // modelo: 'modelo3'
+    // },
+    // {
+    //   conteudo: 'Minha prorpriedade é decorada com ()Input',
+    // autoria: 'Angular',
+    // modelo: 'modelo2'
+    // },
+    // {
+    //   conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing',
+    // autoria: 'Angular',
+    // modelo: 'modelo1'
+    // },
   ]
 
   constructor() { }
