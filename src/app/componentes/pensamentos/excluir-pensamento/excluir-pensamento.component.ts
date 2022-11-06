d38658f7257b5e7e926fad84bcd84bf7897841f2
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-11-06 18:05:19 -0300

    dando funcionalidade ao botao de excluir

--- src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
+++ src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -1,3 +1,6 @@
+import { ActivatedRoute, Router } from '@angular/router';
+import { PensamentoService } from './../pensamento.service';
+import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExcluirPensamentoComponent implements OnInit {
 
-  constructor() { }
+
+  pensamento: Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+  constructor(private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
+      this.pensamento = pensamento
+    })
+  }
+
+  excluirPensamento() {
+    if (this.pensamento.id) {
+      this.service.excluir(this.pensamento.id).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
+  }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento'])
   }
 
 }
