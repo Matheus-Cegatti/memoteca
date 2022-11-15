24b26eb0bfb91f5a126b93c3824b8659a1de4d8c
Matheus-Cegatti <cegattimatheus@gmail.com>
2022-11-15 15:56:05 -0300

    Começando a configurar a aba de favoritos

--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -11,7 +11,7 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) { }
 
-  listar(pagina: number, filtro: string):Observable<Pensamento[]>{
+  listar(pagina: number, filtro: string, favoritos: boolean):Observable<Pensamento[]>{
 
     const itensPorPagina = 6;
 
@@ -24,10 +24,15 @@ export class PensamentoService {
       params = params.set("q", filtro)
     }
 
+    if(favoritos) {
+      params = params.set("favorito", true)
+    }
+
     //GET /posts?_page=7&_limit=20
     // return this.http.get<Pensamento[]>(`${this.API}?_page=${pagina}&_limit=${itensPorPagina}`)
     return this.http.get<Pensamento[]>(this.API, { params })
   }
+
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
     return this.http.post<Pensamento>(this.API, pensamento)
